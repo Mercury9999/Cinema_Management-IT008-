ebf30366fbef56668d5b0caa4b065a12c40cdbc6
vantri206 <vantri0605@gmail.com>
2024-11-05 10:59:48 +0700

    add database

--- CinemaManagement/obj/Debug/net8.0-windows/CinemaManagement.AssemblyInfo.cs
+++ CinemaManagement/obj/Debug/net8.0-windows/CinemaManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d3b14335feb5d445d0c12db44efec732396f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CinemaManagement/obj/Debug/net8.0-windows/View/LoginWindow/LoginWindow.g.i.cs
+++ CinemaManagement/obj/Debug/net8.0-windows/View/LoginWindow/LoginWindow.g.i.cs
@@ -104,7 +104,7 @@ namespace CinemaManagement.View.LoginWindow {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CinemaManagement;component/view/loginwindow/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CinemaManagement;V1.0.0.0;component/view/loginwindow/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\LoginWindow\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
