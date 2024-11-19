60a840814a97e3b600db4855bcfec922ff14d39c
Pham Hung <hungpham3050@gmail.com>
2024-11-19 22:56:10 +0700

    Add database

--- CinemaManagement/obj/Debug/net48/CinemaManagement.AssemblyInfo.cs
+++ CinemaManagement/obj/Debug/net48/CinemaManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8b464cf8f3a53610f159a383339c0119d165a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8347a6481cdd7e6ef25ee3e713e0e136a69a7e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
