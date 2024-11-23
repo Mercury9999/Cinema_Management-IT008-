bc98ab5610e0699cb679b29e7f03b744ff89567f
Pham Hung <hungpham3050@gmail.com>
2024-11-23 17:25:36 +0700

    Update QuanLyPhim, QuanLySuatChieu, ThemSuatChieu, ThongTinPhim, add YesNoMessageBox, sua bug SearchBox

--- CinemaManagement/obj/Debug/net48/CinemaManagement.AssemblyInfo.cs
+++ CinemaManagement/obj/Debug/net48/CinemaManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1700ef3ad09ec83c33375fb15b0d0860ffbdf797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433eda29fb136e8855982ea77a1fc4fbdfb77a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
