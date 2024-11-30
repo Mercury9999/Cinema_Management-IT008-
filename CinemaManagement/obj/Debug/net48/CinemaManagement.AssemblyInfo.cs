7795423674b3dbe055246d4659ab7d66f3cc7911
Pham Hung <hungpham3050@gmail.com>
2024-11-30 17:05:37 +0700

    Adjustment

--- CinemaManagement/obj/Debug/net48/CinemaManagement.AssemblyInfo.cs
+++ CinemaManagement/obj/Debug/net48/CinemaManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b3c4bd11192b7b9c26c66afdb909c8bb8a82bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5353c52e63be75d45f1a61824fffe0ba06ca0456")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
