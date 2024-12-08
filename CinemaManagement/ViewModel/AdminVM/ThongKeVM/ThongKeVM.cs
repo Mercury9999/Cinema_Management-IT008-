22e3b55752c63bd8ecef1cf34e0041cd42237d97
Pham Hung <hungpham3050@gmail.com>
2024-12-08 18:47:54 +0700

    chuaxongVM

---
+++ CinemaManagement/ViewModel/AdminVM/ThongKeVM/ThongKeVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Threading.Tasks;
+using CinemaManagement.DTOs;
+using System.Windows.Controls;
+using System.Runtime.CompilerServices;
+
+namespace CinemaManagement.ViewModel.AdminVM.ThongKeVM
+{
+    public partial class ThongKeVM :BaseViewModel
+    {
+        
+
+
+
+    }
+}
